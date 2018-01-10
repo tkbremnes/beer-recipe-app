b2e8b2030cddf0b4c628c4d6a887496cc8354803
Trond Kjetil Bremnes <tkbremnes@gmail.com>
2018-01-10 19:51:58 +0100

    Experiments in styling simple links

--- src/Components/Calculators/index.js
+++ src/Components/Calculators/index.js
@@ -12,9 +12,29 @@ class Calculators extends Component {
             <Page>
                 <Card header="Calculators">
                     <ul className="Calculator-linkList">
-                        <li><Link to="/calculators/alcohol">ABV calculator</Link></li>
-                        <li><Link to="/calculators/strike_water">Strike water calculator</Link></li>
-                        <li><Link to="/calculators/priming_sugar">Priming sugar calculator</Link></li>
+                        <li>
+                            <Link to="/calculators/alcohol">
+                                <Card header="ABV calculator">
+                                    Estimates the alcohol contents of the fluid, using original and final gravity.
+                                </Card>
+                            </Link>
+                        </li>
+
+                        <li>
+                            <Link to="/calculators/strike_water">
+                                <Card header="Strike water calculator">
+                                    Calculates temperature of water.
+                                </Card>
+                            </Link>
+                        </li>
+
+                        <li>
+                            <Link to="/calculators/priming_sugar">
+                                <Card header="Priming sugar calculator">
+                                    Calculates amount of sugar needed for carbonation.
+                                </Card>
+                            </Link>
+                        </li>
                     </ul>
                 </Card>
             </Page>
